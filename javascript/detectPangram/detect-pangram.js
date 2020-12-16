c829f49615ef5dc85ddcb5985222d9d5839092f0
Alexander Whan <whan.alexander@gmail.com>
2020-12-15 19:45:38 -0800

    Added code solution for isPangram

---
+++ javascript/detectPangram/detect-pangram.js
@@ -0,0 +1,8 @@
+const isPangram = string => {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  let count = 0;
+  for (let letter of alphabet) {
+    string.toLowerCase().indexOf(letter) > -1 ? count++ : count;
+  }
+  return count >= 26;
+};
